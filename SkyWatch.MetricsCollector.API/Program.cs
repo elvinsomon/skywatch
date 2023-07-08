e48736f485f3f79326127a56f2856835ea768d6e
ElvinSomon <esomon@hotmail.com>
2023-07-08 19:59:43 -0400

    Agregar logger al MetricsCollector API

--- SkyWatch.MetricsCollector.API/Program.cs
+++ SkyWatch.MetricsCollector.API/Program.cs
@@ -1,31 +1,57 @@
+using Serilog;
 using SkyWatch.MetricsCollector.Core;
 using SkyWatch.MetricsCollector.Core.Contracts;
 using SkyWatch.MetricsCollector.Infrastructure.DataBase.Mock;
 
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json",
+        true)
+    .Build();
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(configuration)
+    .CreateBootstrapLogger();
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Host.UseSerilog();
 
-builder.Services.AddTransient<IMetricRecordRepository, MetricRecordRepositoryMock>();
-builder.Services.AddTransient<MetricCollectorService>();
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+Log.Information("Sky Watch. Metrics Collector API. Starting up");
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    builder.Services.AddTransient<IMetricRecordRepository, MetricRecordRepositoryMock>();
+    builder.Services.AddTransient<MetricCollectorService>();
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Sky Watch. Metrics Collector API. Unhandled exception");
+}
+finally
+{
+    Log.Information("Sky Watch. Metrics Collector API. Shut down complete");
+    Log.CloseAndFlush();
+}
